6659489b724ce547f5222682b5c328d94d841c04
chamchi0809 <seoshi999@gmail.com>
2025-11-30 18:09:16 +0900

    add hit area

--- src/common/components/entity-views/RhythmView.tsx
+++ src/common/components/entity-views/RhythmView.tsx
@@ -12,15 +12,27 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   gap: 1px;
+  border-radius: 8px;
+  overflow: hidden;
 `;
 
 const Lane = styled.div`
   display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
 
   width: 10%;
   height: 100%;
 
   background-color: rgb(0 0 0 / 0.5);
+  border-radius: 8px;
+`;
+
+const HitArea = styled.div`
+  width: 100%;
+  height: 64px;
+  border-radius: 8px;
+  background-color: rgba(255, 255, 255, 0.2);
 `;
 
 export default function RhythmView({ entity }: { entity: Entity }) {
@@ -30,10 +42,18 @@ export default function RhythmView({ entity }: { entity: Entity }) {
     <group ref={rhythmViewRef}>
       <Html fullscreen>
         <Container>
-          <Lane />
-          <Lane />
-          <Lane />
-          <Lane />
+          <Lane>
+            <HitArea />
+          </Lane>
+          <Lane>
+            <HitArea />
+          </Lane>
+          <Lane>
+            <HitArea />
+          </Lane>
+          <Lane>
+            <HitArea />
+          </Lane>
         </Container>
       </Html>
     </group>

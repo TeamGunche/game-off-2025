d4d2435a1879036750df8b0e6129649aa7d2e6f3
최지원 <66864266+chamchi0809@users.noreply.github.com>
2025-12-01 13:27:57 +0900

    tweak minDst

--- src/common/systems/updateInteractionMoveInput.ts
+++ src/common/systems/updateInteractionMoveInput.ts
@@ -3,6 +3,7 @@ import { IsInteracting } from "@/common/traits/IsInteracting.ts";
 import { IsPlayer } from "@/common/traits/IsPlayer.ts";
 import { MoveInput } from "@/common/traits/MoveInput.ts";
 import { RootRef } from "@/common/traits/RootRef.ts";
+import { IsBattle } from "@/common/traits/IsBattle.ts";
 
 export const updateInteractionMoveInput = () => {
   const opponent = world.queryFirst(IsInteracting);
@@ -11,7 +12,7 @@ export const updateInteractionMoveInput = () => {
   world.query(IsPlayer, MoveInput, RootRef).updateEach(([, moveInput, player]) => {
     const dst = Math.abs(opponent.get(RootRef)!.position.x - player.position.x);
     const dir = opponent.get(RootRef)!.position.x > player.position.x ? -1 : 1;
-    const minDst = 4;
+    const minDst = opponent.has(IsBattle) ? 6 : 1;
 
     if (dst < minDst) {
       moveInput.x = dir;

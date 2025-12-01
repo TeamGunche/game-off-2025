622c799fa79d1888f220452c507cc283fcdd3a5c
최지원 <66864266+chamchi0809@users.noreply.github.com>
2025-12-01 18:19:55 +0900

    prevent scrolling when player is moving

--- src/common/systems/scrollNotes.ts
+++ src/common/systems/scrollNotes.ts
@@ -1,9 +1,14 @@
 import { world } from "@/common/world.ts";
 import { RhythmNote } from "@/common/traits/RhythmNote.ts";
 import { scrollTime } from "@/common/defs/scrollTime.ts";
+import { IsPlayer } from "@/common/traits/IsPlayer.ts";
+import { MoveInput } from "@/common/traits/MoveInput.ts";
 
 export const scrollNotes = (delta: number) => {
+  const player = world.queryFirst(IsPlayer);
+
   world.query(RhythmNote).updateEach(([note]) => {
+    if (player?.get(MoveInput)?.x !== 0) return;
     note.position -= delta / scrollTime;
   });
 };
